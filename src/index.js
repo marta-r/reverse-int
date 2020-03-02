7cc0289530ba6f3dc7b23392b0d9a6f7576bccb8
Marta Rudkouskaya <lvlartik@gmail.com>
2020-03-02 17:50:11 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('');
+    arr.reverse();
+    while (arr[0] == 0) {
+        arr.shift();
+    }
+    while (arr[arr.length-1]=='-'){
+        arr.pop();
+    }
+    return +arr.join('');
 }
